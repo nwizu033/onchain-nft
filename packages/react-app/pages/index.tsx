df0043be02adbe30c145716effbdad3aa55dffb6
chinwizu <chinwianthony@gmail.com>
2023-05-04 12:01:59 +0100

    sort deploy error

--- packages/react-app/pages/index.tsx
+++ packages/react-app/pages/index.tsx
@@ -1,17 +1,24 @@
 import abi from 'utils/abi.json';
 import { useSigner } from "wagmi";
 import { ethers } from 'ethers';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 export default function Home() {
   const contractAddress = "0x3c5aD009d2e9f11A06c6ec31377D612C81f6b224"
   const { data: signer, isError, isLoading } = useSigner();
-  const [name, setName] = useState();
-  const [desc, setDesc] = useState();
+  const [name, setName] = useState('');
+  const [desc, setDesc] = useState('');
   const [yourNft, setYourNft] = useState();
-  const [tokenId, setTokenId] = useState();
+  const [tokenId, setTokenId] = useState('');
    
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    setName(event.target.value);
+  }
+
+  const handleChange2 = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    setDesc(event.target.value);
+  }
 
   const seeNft = async() =>{
     if(signer) {
@@ -35,8 +42,8 @@ export default function Home() {
       
       <div className='flex flex-col lg:w-[600px]'>
         <p className=' text-3xl text-center font-medium mt-4'>MINT YOUR NFT</p>
-        <input className=' my-4 py-3 bg-transparent border-black border-2 rounded-lg px-4' onChange={(e) =>setName(e.target.value)} type='text' placeholder='Enter your name' />
-        <input className=' my-4 py-3 bg-transparent border-black border-2 rounded-lg px-4' onChange={(e) =>setDesc(e.target.value)} type='text' placeholder='Enter descriptiion'/>
+        <input className=' my-4 py-3 bg-transparent border-black border-2 rounded-lg px-4' onChange={handleChange} type='text' placeholder='Enter your name' />
+        <input className=' my-4 py-3 bg-transparent border-black border-2 rounded-lg px-4' onChange={handleChange2} type='text' placeholder='Enter descriptiion'/>
         <button onClick={mintNft} className=' my-4 py-3 bg-yellow-200 border-black border-2 rounded-lg px-4 text-xl font-medium tracking-widest'>Mint</button>
       </div>
       
